21df119bee144e78bda0c16bc1fe025780f00829
Kim Dave Torres <torres.kim.dave@gmail.com>
2020-11-06 16:32:35 +0800

    Initial commit

---
+++ js/default-assets/_login_identity.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('#lgn_submit').click(function() {
+        if($('#u_name').val() == '') {
+            $('#u_name').css('border', '1px solid #dc3545');
+            $('#p_wd').css('border', '1px solid #dc3545');
+            return false;
+        }else if($('#p_wd').val() == '') {
+            $('#p_wd').css('border', '1px solid #dc3545');
+            return false;
+        }else {
+            if($('#u_name').val() == 'kimdavetorres' && $('#p_wd').val() == '123456789012') {
+                $('form').attr({
+                    'action':'admin.html',
+                    'method':'POST'
+                });
+            }else if($('#u_name').val() == 'jayautentico' && $('#p_wd').val() == '123456789012') {
+                $('form').attr({
+                    'action':'ticket-in-charge.html',
+                    'method':'POST'
+                });
+            }else {
+                alert("No existing account!");
+                return false;
+            }
+        }
+    });
+});
