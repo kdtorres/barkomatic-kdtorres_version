9200a10f21747470503ec7d127c256184161302a
Kim Dave Torres <torres.kim.dave@gmail.com>
2020-11-06 17:11:27 +0800

    login rechange

--- js/default-assets/login.js
+++ js/default-assets/login.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
             return false;
         }else {
             if($('#u_name').val() == 'kimdavetorres' && $('#p_wd').val() == '123456789012') {
-                // $('form').attr({
-                //     'action':'https://kimdavetorres.github.io/vg-shipping-lines/admin.html'
-                // });
-                window.location.href = 'https://kimdavetorres.github.io/vg-shipping-lines/admin.html';
+                $('form').attr({
+                    'action':'https://kimdavetorres.github.io/vg-shipping-lines/admin.html',
+                    'method':'POST'
+                });
             }else if($('#u_name').val() == 'jayautentico' && $('#p_wd').val() == '123456789012') {
                 $('form').attr({
                     'action':'ticket-in-charge.html',
